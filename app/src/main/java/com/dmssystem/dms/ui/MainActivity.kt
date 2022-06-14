c7bf4cfe188325f68e346889a9efb76617479834
lokified <lsheldon645@gmail.com>
2022-06-14 17:31:28 +0300

    Initial commit

---
+++ app/src/main/java/com/dmssystem/dms/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dmssystem.dms.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dmssystem.dms.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
