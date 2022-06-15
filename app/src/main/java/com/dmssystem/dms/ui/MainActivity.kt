2e89c81fe2a7001c182731d187a966be4e4484bf
lokified <lsheldon645@gmail.com>
2022-06-15 21:41:49 +0300

    add splash screen and animations to the logo

--- app/src/main/java/com/dmssystem/dms/ui/MainActivity.kt
+++ app/src/main/java/com/dmssystem/dms/ui/MainActivity.kt
@@ -1,10 +1,24 @@
 package com.dmssystem.dms.ui
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dmssystem.dms.R
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+
+        fun newIntent(context: Context): Intent {
+
+            val intent = Intent(context, MainActivity::class.java)
+
+            return intent
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/dmssystem/dms/ui/splash/SplashActivity.kt
@@ -0,0 +1,65 @@
+package com.dmssystem.dms.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
+import com.dmssystem.dms.R
+import com.dmssystem.dms.databinding.ActivitySplashBinding
+import com.dmssystem.dms.ui.MainActivity
+import kotlin.concurrent.thread
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        Handler().postDelayed( Runnable {
+            setAnimations()
+        }, 1000)
+
+
+        Handler().postDelayed(Runnable {
+            navigateToMainActivity()
+        },  2000)
+
+    }
+
+
+    private fun navigateToMainActivity() {
+
+        val transitionIntent = MainActivity.newIntent(this)
+
+        val logoIcon: ImageView = findViewById(R.id.app_logo)
+        val logoPair = Pair.create(logoIcon as View, "tLogoIcon")
+
+        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, logoPair)
+
+        ActivityCompat.startActivity(this, transitionIntent, options.toBundle())
+        finish()
+    }
+
+
+    private fun setAnimations() {
+
+        val logoAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_logo_anim)
+        val logoNameAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_logo_name_anim)
+
+        binding.appLogo .visibility = View.VISIBLE
+        binding.logoName.visibility = View.VISIBLE
+
+        binding.appLogo.startAnimation(logoAnimation)
+        binding.logoName.startAnimation(logoNameAnimation)
+    }
+}
