b30678d54fd26f3b38bc4316a273e9ff45bb2bda
lokified <lsheldon645@gmail.com>
2022-06-16 23:27:55 +0300

    add a login layout

--- app/src/main/java/com/dmssystem/dms/ui/lookup/LookUpFragment.kt
+++ app/src/main/java/com/dmssystem/dms/ui/lookup/LookUpFragment.kt
@@ -1,15 +1,20 @@
 package com.dmssystem.dms.ui.lookup
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.dmssystem.dms.R
 import com.dmssystem.dms.databinding.FragmentLookUpBinding
+import com.dmssystem.dms.util.VerifyPopup
 
 class LookUpFragment : Fragment() {
     private lateinit var binding: FragmentLookUpBinding
+
+    private val popup = VerifyPopup()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,4 +26,23 @@ class LookUpFragment : Fragment() {
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.lookupBtn.setOnClickListener {
+
+            Handler().postDelayed(Runnable {
+
+                popup.dialog.dismiss()
+
+                val action = LookUpFragmentDirections.actionLookUpFragmentToLoginFragment()
+                findNavController().navigate(action)
+
+            }, 6000)
+
+            popup.createVerifyPopup(context)
+            popup.timeCountdown.start()
+        }
+    }
 }
